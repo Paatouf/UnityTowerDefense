e83caa8693e260a838c72a882328b857ea8c5dd1
vchagnard <v.chagnard@gmail.com>
2017-06-30 16:09:01 +0200

    [CHANGED] Mainmenu improved, and levelA,B & C added

--- TowerDefense/Assets/Scripts/MainMenu.cs
+++ TowerDefense/Assets/Scripts/MainMenu.cs
@@ -1,25 +1,114 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 
 {
-    public string levelToLoad = "LevelA";
+    private string levelToLoad = "";
 
-	public void Play ()
+    [Header("LevelTab")]
+    public GameObject LevelTab;
+    private bool LevelASelected = false;
+    private bool LevelBSelected = false;
+    private bool LevelCSelected = false;
+    public Text ConfirmText;
+    public GameObject GoButton;
+
+    [Header("StatsTab")]
+    public GameObject StatsTab;
+
+
+
+    private void Start()
     {
-        levelToLoad = "LevelA";
-        SceneManager.LoadScene(levelToLoad);
+        ResetLevelTab();
+    }
+
+    public void Play ()
+    {
+        StatsTab.SetActive(false);
+
+        if (LevelTab.activeInHierarchy)
+        {
+            ResetLevelTab();
+            LevelTab.SetActive(false);
+        }
+        else
+        {
+            LevelTab.SetActive(true);
+        }
 	}
 
-    public void Play2()
+    public void Stats()
     {
-        levelToLoad = "LevelB";
+        LevelTab.SetActive(false);
+
+        if (StatsTab.activeInHierarchy)
+        {
+            StatsTab.SetActive(false);
+        }
+        else
+        {
+            StatsTab.SetActive(true);
+        }
+    }
+    public void SelectLevelA()
+    {
+        LevelASelected = true;
+        LevelBSelected = false;
+        LevelCSelected = false;
+
+        ConfirmText.text = "ARE YOU SURE YOU WANT TO PLAY THE LEVEL A";
+        GoButton.SetActive(true);
+    }
+    public void SelectLevelB()
+    {
+        LevelASelected = false;
+        LevelBSelected = true;
+        LevelCSelected = false;
+
+        ConfirmText.text = "ARE YOU SURE YOU WANT TO PLAY THE LEVEL B";
+        GoButton.SetActive(true);
+    }
+    public void SelectLevelC()
+    {
+        LevelASelected = false;
+        LevelBSelected = false;
+        LevelCSelected = true;
+
+        ConfirmText.text = "ARE YOU SURE YOU WANT TO PLAY THE LEVEL C";
+        GoButton.SetActive(true);
+    }
+
+    public void Go()
+    {
+        if(LevelASelected)
+        {
+            levelToLoad = "LevelA";
+        }
+        else if(LevelBSelected)
+        {
+            levelToLoad = "LevelB";
+        }
+        else if(LevelCSelected)
+        {
+            levelToLoad = "LevelC";
+        }
+
         SceneManager.LoadScene(levelToLoad);
     }
 
+    public void ResetLevelTab()
+    {
+        GoButton.SetActive(false);
+        ConfirmText.text = "";
+        LevelASelected = false;
+        LevelBSelected = false;
+        LevelCSelected = false;
+    }
     public void Quit ()
     {
         Application.Quit();
